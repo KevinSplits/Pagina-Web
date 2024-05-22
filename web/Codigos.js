820b43acc7fb89eee6d6d073370227caba4f804b
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2024-05-22 01:59:37 -0500

    act10

--- web/Codigos.js
+++ web/Codigos.js
@@ -296,8 +296,6 @@ function mostrarSigno(){
 
     if((dia>=21&&mes==3)||(dia<=20&&mes==4)){
         signo = 'Aries';
-        descSigno = 'Descripción de Aries';
-        imgSigno = 'imagenes/aries.jpg';
         descSigno = 'Aries forma parte de los signos cardinales y al mismo tiempo es un signo de fuego;'+
         ' también es el primer signo del zodíaco, precisamente por eso, simboliza el inicio, la creación.'+
         ' Se caracteriza por ser una persona rebosante de energía y entusiasmo; avanzada y aventurera, adora la libertad, los retos y las nuevas ideas.\n'+
@@ -307,8 +305,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Aries.png';
     }else if((dia>=24&&mes==9)||(dia<=23&&mes==10)){
         signo = 'Libra';
-        descSigno = 'Descripción de Libra';
-        imgSigno = 'imagenes/libra.jpg';
         descSigno = 'Libra es un signo cardinal y de aire, se encuentra además entre los signos más refinados del zodíaco; '+
         'tiene elegancia, encanto, diplomacia y buen gusto, ama la belleza, es muy curioso por naturaleza y odia los conflictos.'+
         ' Sus puntos negativos a veces son la frivolidad y un carácter voluble.\n'+' Al igual que su propio arquetipo: la balanza,'+
@@ -319,8 +315,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Libra.png';
     }else if((dia>=21&&mes==4)||(dia<=21&&mes==5)){
         signo = 'Tauro';
-        descSigno = 'Descripción de Tauro';
-        imgSigno = 'imagenes/tauro.jpg';
         descSigno = 'Tauro pertenece a los signos fijos y simultáneamente es un signo de tierra. La proyección del Sol en su nacimiento suele'+
         ' influir para que sean personas firmes, decididas y constantes en varios sentidos. También adoran sentir seguridad, por eso la buscan tanto, es como'+
         ' una necesidad constante en sus vidas. Un Tauro es un ser decidido, pragmático y tiene una enorme fuerza de voluntad. Tiende a las ideas conservadoras,'+
@@ -330,8 +324,10 @@ function mostrarSigno(){
         imgSigno = 'imagen/Tauro.png';
     }else if((dia>=24&&mes==10)||(dia<=22&&mes==11)){
         signo = 'Escorpio';
-        descSigno = 'Descripción de Escorpio';
-        imgSigno = 'imagenes/escorpio.jpg';
+        descSigno = 'Escorpio es un signo fijo y de agua; su potencia y energía emocional son únicas en todo el zodíaco. Tiene mucha imaginación e intuición, además de una gran capacidad para el análisis,'+
+        ' fuerza de voluntad y firmeza, aunque también es muy sensible y emocional consigo mismo y con el entorno. Además, Escorpio desea ejercer el control, acumulando poder; suele tener una profunda inteligencia fruto de su poder de concentración.\n'+
+        ' Escorpio tiende a ir a la raíz, a lo esencial y a lo que pertenece al mundo inconsciente. Un Escorpión es inmensamente enérgico y su carácter puede dar lugar a grandes ventajas o no menos grandes peligros para los demás.'+
+        ' De hecho, sus puntos negativos son la agresión, los celos y el resentimiento. No soporta la deslealtad y tampoco perdona una ofensa; es más, seguramente busque venganza... y es muy mal enemigo, así que cuidadito con hacerle daño.';
         imgSigno = 'imagen/Escorpio.png';
     }else if((dia>=22&&mes==5)||(dia<=21&&mes==6)){
         signo = 'Géminis';
@@ -346,8 +342,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Geminis.png';
     }else if((dia>=23&&mes==11)||(dia<=21&&mes==12)){
         signo = 'Sagitario';
-        descSigno = 'Descripción de Sagitario';
-        imgSigno = 'imagenes/sagitario.jpg';
         descSigno = 'Sagitario pertenece a los signos mutables y su elemento es el fuego; es uno de los signos más resplandecientes y positivos del zodíaco.'+
         ' También es versátil, adora las aventuras y buscar nuevos horizontes, ya que tiene una mente abierta a nuevas ideas y experiencias y mantiene una actitud decidida ante la adversidad;'+
         ' además, frecuentemente la suerte le acompaña. Asimismo, son personas a quienes les encanta empezar nuevos proyectos y aprender constantemente cosas nuevas.'+
@@ -357,8 +351,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Sagitario.png';
     }else if((dia>=21&&mes==6)||(dia<=23&&mes==7)){
         signo = 'Cáncer';
-        descSigno = 'Descripción de Cáncer';
-        imgSigno = 'imagen/cancer.jpg';
         descSigno = 'Cáncer es un signo cardinal y comprendido dentro de los signos de agua. De los signos zodiacales, su carácter es el menos claro; puede ser desde retraído, insociable y pelma,'+
         ' hasta deslumbrante, atractivo y admirado por los demás. A veces es demasiado soñador, por eso equivoca el mundo real con la utopía que ha construido en su cabeza: el refugio de las fantasías que adora.\n'+
         ' Los Cáncer tienen una memoria extraordinaria y además poseen un talento innato para las artes y las letras notable. Además, son ambiciosos y pueden cambiar sin dificultad de profesión, de amistades, etc.\n'+
@@ -366,8 +358,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Cancer.png';
     }else if((dia>=22&&mes==12)||(dia<=20&&mes==1)){
         signo = 'Capricornio';
-        descSigno = 'Descripción de Capricornio';
-        imgSigno = 'imagenes/capricornio.jpg';
         descSigno = 'Capricornio es un signo cardinal y de tierra, y uno de los signos del zodíaco más constante, sólido y apacible. También se caracteriza por ser prudente y práctico en todos los asuntos que le conciernen.'+
         ' Sus aspectos más negativos tienden hacia el pesimismo, la fijeza y la melancolía. En general, son personas trabajadoras, responsables y dispuestas a persistir lo que haga falta para conseguir su objetivo; además son muy fiables y '+
         'cuentan con unas elevadas dosis de paciencia y resistencia para avanzar hacia sus objetivos; en este sentido, su capacidad para la gestión resulta asombrosa. Por otra parte, no soportan la falta de honradez ni de disciplina.\n'+
@@ -375,8 +365,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Capricornio.png';
     }else if((dia>=24&&mes==7)||(dia<=23&&mes==8)){
         signo = 'Leo';
-        descSigno = 'Descripción de Leo';
-        imgSigno = 'imagenes/leo.jpg';
         descSigno = 'El signo de Leo es fijo y de fuego, también el signo más dominante del zodíaco. Creativo y abierto, tiene ambición, valor, fuerza, autonomía y total seguridad en sí mismo: sabe dónde quiere llegar y nada ni nadie podrá evitarlo.'+
         ' En contrapartida, sus puntos negativos pueden ser tantos como las virtudes que tiene: vanidad, egocentrismo, arrogancia, impostura y un genio de mil demonios, entre otros defectos.\n'+
         ' Los Leones son entusiastas, creativos y muchas veces comprensivos con las circunstancias de los demás; adoran los lujos y la aventura; correr riesgos les motiva. También se caracterizan por tener un elevado concepto de todo, especialmente, de sí mismos,'+
@@ -384,8 +372,6 @@ function mostrarSigno(){
         imgSigno = 'imagen/Leo.png';
     }else if((dia>=21&&mes==1)||(dia<=19&&mes==2)){
         signo = 'Acuario';
-        descSigno = 'Descripción de Acuario';
-        imgSigno = 'imagenes/acuario.jpg';
         descSigno = 'Acuario es un signo fijo y de aire, y sin duda, es el signo con mayor capacidad para la invención de toda la rueda zodiacal. Simpático, original y brillante, Acuario también es un signo muy humanitario, al mismo tiempo que independiente e intelectual.'+
         ' Sus puntos negativos se encuentran en su inestabilidad e imprecisión y en su tendencia a llevar la contraria casi por sistema.\n'+
         ' Las personas nacidas Acuario hacen gala de de una sinceridad e idealismo a prueba de bombas. Amantes de todo lo original, tienen montones de ideas nuevas agolpadas en sus mentes, lo que pasa es que cuando van a poner en marcha una de ellas, sale otra,'+
@@ -393,16 +379,12 @@ function mostrarSigno(){
         imgSigno = 'imagen/Acuario.png';
     }else if((dia>=24&&mes==8)||(dia<=23&&mes==9)){
         signo = 'Virgo';
-        descSigno = 'Descripción de Virgo';
-        imgSigno = 'imagenes/virgo.jpg';
         descSigno = 'Virgo es un signo mutable y de tierra; representado por una virgen, es un signo caracterizado por su espíritu crítico, precisión, reserva, paciencia y convencionalismo. También es lógico, metódico y aplicado, le gusta aprender y es capaz de analizar las situaciones más complejas con una claridad pasmosa.\n'+
         ' Como es el perfeccionista del zodíaco, de vez en cuando cae en obsesiones de todo tipo: desde la obsesión por el orden, pasando por la limpieza, hasta llegar incluso a la obsesión por los detalles. Su sentido de la responsabilidad está muy desarrollado, por eso odia la improvisación y la frivolidad, además, para desarrollarse plenamente necesita sentirse seguro.\n'+
         ' Los Virgo son amigos de sus amigos; si necesitas su ayuda, pídesela y hará todo lo que esté en sus manos por ayudarte. Además, su visión seguramente sea la más acertada, así que puedes estar seguro de que te servirá de algo.';
         imgSigno = 'imagen/Virgo.png';
     }else if((dia>=20&&mes==2)||(dia<=20&&mes==3)){
         signo = 'Piscis';
-        descSigno = 'Descripción de Piscis';
-        imgSigno = 'imagenes/piscis.jpg';
         descSigno = 'Piscis es un signo mutable y de agua, también es el último signo del zodiaco, precisamente por eso, es el más rico y complejo de todos. Sensible ante el sufrimiento de los demás, responde con buena voluntad y ganas de ayudar. No le gusta sentirse preso y ni respeta las convenciones,'+
         ' así, por las buenas, aunque tampoco tiende a luchar contra lo establecido, sencillamente, discurre por otro lado.\n Los Piscis tienden a vivir de una manera emocional más que racional, de forma instintiva e intuitiva más que de forma lógica.'+
         ' Les cuesta mucho transmitir lo que perciben, no saben expresarlo con palabras sino con acciones... Una clave para los Piscis es cómo contactan con su sensibilidad. Además, aunque suele decirse de Piscis que es el signo de la ingenuidad, cuando es su respuesta ante el mundo de las sensaciones que contempla'+
@@ -412,22 +394,25 @@ function mostrarSigno(){
     
     const mostrarSig = document.getElementById('result1');
     mostrarSig.innerHTML = `<div class="resultado-signo">
-                                <img src="${imgSigno}" alt="${signo}" class="imagen-signo">
-                                <div class="descrip-contain-signo">
-                                    <div class="nombre-signo">${signo}</div>
-                                    <div class="descripcion-signo">${descSigno}</div>
-                                    </div>
-                           </div>`;
+                        <img src="${imgSigno}" alt="${signo}" class="imagen-signo">
+                        <div class="descrip-contain-signo">
+                            <div class="nombre-signo">${signo}</div>
+                                <div class="descripcion-signo" id="descripcion-signo" onmouseover="agrandarDescripSigno()" onmouseout="reducirDescripSigno()">
+                                ${descSigno}</div>
+                            </div>
+                        </div>`;
 }
+
 function agrandarDescripSigno() {
     var descripcionSigno = document.getElementById('descripcion-signo');
-    descripcionSigno.style.fontSize = '30px'; // Cambia el tamaño de la fuente a 20px al pasar el mouse sobre la descripción
+    descripcionSigno.style.fontSize = '30px'; 
 }
 
 function reducirDescripSigno() {
     var descripcionSigno = document.getElementById('descripcion-signo');
-    descripcionSigno.style.fontSize = '16px'; // Restaura el tamaño de la fuente a 16px al sacar el mouse de la descripción
+    descripcionSigno.style.fontSize = '16px'; 
 }
+
 function reestablecerSigno() {
     document.getElementById('result1').innerHTML = "";
 }
