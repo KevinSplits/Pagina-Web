322a29938205f7454e04014c29ae3d1578cfd369
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2024-06-17 22:02:37 -0500

    a

---
+++ web/insertar.php
@@ -0,0 +1,11 @@
+<?php
+	include ('conexion.php');
+	if(isset($_POST['nombre']) && !empty($_POST['nombre'])
+	 && isset($_POST['pass']) && !empty($_POST['pass'])){
+		$con = mysqli_connect($host,$user,$pass,$db) or die ("Problemas al conectar");
+		mysqli_query($con,"INSERT INTO codigo (nombre,pass) VALUES ('$_POST[nombre]','$_POST[pass]')");
+		echo "datos insertados";
+	} else {
+		echo "Problemas al insertar datos";
+	}
+?>
