a7042a6235ccbf51b0da2957a3479fda385c26a9
KevinSplits <130183913+KevinSplits@users.noreply.github.com>
2024-06-17 20:37:17 -0500

    1

--- web/conexion.php
+++ web/conexion.php
@@ -1,15 +1,11 @@
 <?php
-include('conexion.php');
-if(isset($_POST['nombre']) && !empty($_POST['nombre']) && 
-    isset($_POST['pass']) && !empty($_POST['pass']))
-{
-    $con=mysqli_connect($host,$user,$pass,$db) or die("
-    problemas al conectar");
-    mysqli_query($con,"INSERT INTO codigo(nombre,pass) VALUES('$_POST[nombre]','$_POST[pass]')");
-    echo "datos insertados";
+$host="localhost";
+$user="root";
+$pass="";
+$db="db_datamining";
 
-}else {
-    echo "Problemas al insertar";
-}   
+
+
+
 
 ?>
